0ef66efc2202f922fc7ada567ee05981767116ad
Haye <hayedewit98@gmail.com>
2025-03-13 10:49:31 +0100

    Update useAlgoliaSearch.ts

--- src/runtime/composables/useAlgoliaSearch.ts
+++ src/runtime/composables/useAlgoliaSearch.ts
@@ -30,7 +30,7 @@ export function useAlgoliaSearch (indexName?: string) {
       if (config.public.algolia.useFetch) {
         nuxtApp.$algolia.transporter.requester = (await import("@algolia/requester-fetch").then((lib) => lib.default || lib)).createFetchRequester();
       } else {
-        nuxtApp.$algolia.transporter.requester = (await import('@algolia/requester-node-http').then(lib => lib.default || lib)).createNodeHttpRequester()
+        nuxtApp.$algolia.transporter.requester = (await import('@algolia/requester-node-http').then(lib => lib.default || lib)).createHttpRequester()
       }
     }
 
